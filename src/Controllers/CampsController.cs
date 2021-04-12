0d13029f6ff3f2a588ae9da71efebfabb266caa1
Rehan Memon <rehan4747@gmail.com>
2021-04-12 14:42:02 +0530

    Using GET for Collections Through DB

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,36 @@
+﻿using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository _repository;
+
+        public CampsController(ICampRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _repository.GetAllCampsAsync();
+
+                /*return Ok(new { Moniker = "ATL2018", Name = "Atlanta Code Camp" });*/
+                return Ok(results);
+            }
+            catch
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+
+        }
+    }
+}
