b0e1b49e146d81747ad44bb7c7ba50801d7b48eb
Rehan Memon <rehan4747@gmail.com>
2021-04-13 15:28:34 +0530

    Return Models instead of Entities,Get Individual item

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -1,6 +1,9 @@
-﻿using CoreCodeCamp.Data;
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace CoreCodeCamp.Controllers
@@ -9,24 +12,44 @@ namespace CoreCodeCamp.Controllers
     public class CampsController : ControllerBase
     {
         private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
 
-        public CampsController(ICampRepository repository)
+        public CampsController(ICampRepository repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
 
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<ActionResult<CampModel[]>> Get()
         {
             try
             {
                 var results = await _repository.GetAllCampsAsync();
-
                 /*return Ok(new { Moniker = "ATL2018", Name = "Atlanta Code Camp" });*/
-                return Ok(results);
+                return _mapper.Map<CampModel[]>(results); ;
             }
             catch
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+
+        }
+
+
+        [HttpGet("{moniker}")]
+        public async Task<ActionResult<CampModel>> Get(string moniker)
+        {
+            try
+            {
+                var result = await _repository.GetCampAsync(moniker);
+
+                if (result == null) return NotFound();
+
+                return _mapper.Map<CampModel>(result);
+            }
+            catch (Exception)
             {
                 return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
             }
